6410bceaec8a8ba29c73d6a84c398b7bf4c29895
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-12-16 01:54:43 +0100

    added comments

--- PyPass/password_s.py
+++ PyPass/password_s.py
@@ -6,6 +6,7 @@ characters = string.ascii_letters + string.digits
 
 
 def get_password(minimum: int = 15, maximum: int = 20) -> str:
+    """ Generate a single password. """
     length = secret.randint(minimum, maximum)
     while True:
         password = "".join(secret.choice(characters) for _ in range(length))
@@ -15,10 +16,12 @@ def get_password(minimum: int = 15, maximum: int = 20) -> str:
 
 
 def get_passwords(amount: int) -> str:
+    """ Generate multiple passwords. """
     return "\n".join(get_password() for _ in range(amount))
 
 
 def save_password(passwords: str, path: str) -> None:
+    """ Write data to file. """
     try:
         f = open(path, "w+")
     except Exception as e:
@@ -29,4 +32,5 @@ def save_password(passwords: str, path: str) -> None:
 
 
 def meets_basic_requirements(password: str) -> bool:
+    """ Contains at least one uppercase character and one digit. """
     return any(c.isupper() for c in password) and any(c.isdigit() for c in password)
