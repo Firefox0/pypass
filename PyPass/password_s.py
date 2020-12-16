9175a4f9ead8126d3a80aac42493cc247f46896c
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-12-16 01:43:20 +0100

    included python's string module

--- PyPass/password_s.py
+++ PyPass/password_s.py
@@ -1,14 +1,8 @@
 import secrets 
-
+import string
 
 secret = secrets.SystemRandom()
-characters = [
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-            "v", "w", "x", "y", "z",
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-            "V", "W", "X", "Y", "Z",
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-]
+characters = string.ascii_letters + string.digits
 
 
 def get_password(minimum: int = 15, maximum: int = 20) -> str:
