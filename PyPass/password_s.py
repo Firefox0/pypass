b489ae4f3bcfb6256cbf094097f21c71709286e8
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-12-16 01:53:20 +0100

    added basic requirements

--- PyPass/password_s.py
+++ PyPass/password_s.py
@@ -7,6 +7,10 @@ characters = string.ascii_letters + string.digits
 
 def get_password(minimum: int = 15, maximum: int = 20) -> str:
     length = secret.randint(minimum, maximum)
+    while True:
+        password = "".join(secret.choice(characters) for _ in range(length))
+        if meets_basic_requirements(password):
+            break
     return "".join(secret.choice(characters) for _ in range(length))
 
 
@@ -22,3 +26,7 @@ def save_password(passwords: str, path: str) -> None:
         return
     f.write(passwords)
     f.close()
+
+
+def meets_basic_requirements(password: str) -> bool:
+    return any(c.isupper() for c in password) and any(c.isdigit() for c in password)
