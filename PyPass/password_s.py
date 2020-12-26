34a187ee2dd8c1c33e26bf31fcc9a2626e8b4c49
Firefox0 <you@example.com>
2020-12-26 12:52:06 +0100

    added new functions

--- PyPass/password_s.py
+++ PyPass/password_s.py
@@ -1,5 +1,6 @@
 import secrets 
 import string
+import typing
 
 secret = secrets.SystemRandom()
 characters = string.ascii_letters + string.digits
@@ -14,9 +15,24 @@ def get_password(minimum: int = 15, maximum: int = 20) -> str:
             return password
 
 
+def get_passwords_l(amount: int) -> list:
+    """ Get list with passwords. """
+    return [get_password() for _ in range(amount)]
+
+
+def iter_to_str(i: typing.Iterable) -> str:
+    """ Convert iterable to string. """
+    if type(i) is dict:
+        s = "\n".join(i[e] for e in i)
+    else:
+        s = "\n".join(i)
+    return s + "\n"
+
+
 def get_passwords(amount: int) -> str:
-    """ Generate multiple passwords. """
-    return "\n".join(get_password() for _ in range(amount)) + "\n"
+    """ Get passwords as string. """
+    passwords_l = get_passwords_l(amount)
+    return iter_to_str(passwords_l)
 
 
 def save_password(passwords: str, path: str) -> None:
